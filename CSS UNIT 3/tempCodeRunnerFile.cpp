cedbf75935a9a8f5e2d27ad99a78d37dc99f29ce
Shaurya0117 <shaurya7185@gmail.com>
2026-02-02 16:39:59 +0530

    layout 1 and 2

---
+++ CSS UNIT 3/tempCodeRunnerFile.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#using namespace std;
+int main(){
+    int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+    int sum = 0;
+    if(int i = 1; i<= 10; i++){
+        sum += arr[];
+        cout << sum;
+        
+    }
+    
+    return 0;
+}
